13a86669bd0687c62e3a294bb5629862d9958c2d
Taylor <taylornarchibald@gmail.com>
2019-09-05 14:55:54 -0600

    awesome fixes

--- baseline.py
+++ baseline.py
@@ -4,28 +4,53 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 
 class BaselineClassifier(BaseEstimator,ClassifierMixin):
 
-    def __init__(self,param1=True): ## add parameters here
-        self.param1 = param1
+    def __init__(self, example_parameter=True):
+        """ Initialize class with chosen hyperparameters.
+        """
 
-    #This is the function to Train your weights.
-    #In the Baseline Classifier it simply learns what the Most Common Value is.
-    def fit(self,data,labels):
-        flat = labels.flatten().astype(np.int16)
+        self.example_parameter = example_parameter
+
+    def fit(self, X, y):
+        """ This is the function to Train your weights. This Baseline Classifier
+         just finds the mode of your data.
+
+        Args:
+            X (array-like): A 2D numpy array with the training data, excluding targets
+            y (array-like): A 2D numpy array with the training targets
+
+        Returns:
+        """
+        flat = y.flatten().astype(np.int16)
         bins = np.bincount(flat) ### bin the data aka count each class 
         self.most_common = np.argmax(bins) ### grab the class with the highest count
 
-    #Given Novel input it predict an output
-    #each row in data is a row in the dataset or a single data point
-    def predict(self,data):
-        h,w = data.shape
+    def predict(self, X):
+        """ Predict all classes for a dataset
+
+        Args:
+            X (array-like): A 2D numpy array with the training data, excluding targets
+
+        Returns:
+        """
+        h,w = X.shape
         prediction = np.full((h,1),self.most_common) # always guess the most common
         return prediction
 
-    #Returns the Mean(Accuracy) score given input data and labels
-    def score(self,data,labels):
-        h,w = data.shape
-        predictions = self.predict(data)
-        diff = labels.reshape(-1,1) - predictions.reshape(-1,1)
+    def score(self, X, y):
+        """ Return accuracy of model on a given dataset.
+        Args:
+            X (array-like): A 2D numpy array with data, excluding targets
+            y (array-like): A 2D numpy array with targets
+
+        Returns:
+
+        """
+        h,w = X.shape
+        predictions = self.predict(X)
+        diff = y.reshape(-1, 1) - predictions.reshape(-1, 1)
         diff[diff != 0] = 1
         incorrect = np.sum(diff)
         return 1 - (incorrect / h)
+
+if __name__ == "__test__":
+    pass
--- perceptron.py
+++ perceptron.py
@@ -1,34 +1,76 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 
+### NOTE: The only methods you are required to have are:
+#   predict
+#   fit
+#   score
+#   get_weights
+#   They must take at least the parameters below, exactly as specified.
 
 class PerceptronClassifier(BaseEstimator,ClassifierMixin):
-    
-    #### you can add your own params for debug purposes but you need to have
-        # LR --- Learning rate
-        # deterministic -- Deterministic is for grading purposes. When Determinisitic != -1 shuffling is turned off and you train the model for the number of epochs e.g. if determinsitic = 10 then you train the model for 10 epochs without shuffling
-        # shuffle do I shuffle or do I not.
-    def __init__(self,LR=.1,deterministic=-1,shuffle=True):
-        self.LR = LR
+
+    def __init__(self, lr=.1, shuffle=True, initial_weights=None):
+        """ Initialize class with chosen hyperparameters.
+
+        Args:
+            lr (float): A learning rate / step size.
+            shuffle: Whether to shuffle the training data each epoch. DO NOT SHUFFLE for evaluation / debug datasets.
+        """
+        self.lr = lr
         self.shuffle = shuffle
-        self.deterministic = deterministic
-        if self.deterministic != -1:
-            self.shuffle = False
-    ### Fit is Training the model
-    def fit(self,data,labels):
+        self.initial_weights = self.initialize_weights() if not initial_weights else initial_weights
+
+    def fit(self, X, y):
+        """ Fit the data; run the algorithm and adjust the weights to find a good solution
+
+        Args:
+            X (array-like): A 2D numpy array with the training data, excluding targets
+            y (array-like): A 2D numpy array with the training targets
+
+        Returns:
+
+        """
         pass
-    ### After the model is trained you can called predict and it makes a prediction on the given input
-    def predict(self,test):
+
+    def predict(self, X):
+        """ Predict all classes for a dataset X
+
+        Args:
+            X (array-like): A 2D numpy array with the training data, excluding targets
+
+        Returns:
+
+        """
         pass
-    ### you see how well the model does on the given data and labels/targets. Returns Accuracy.
-    def score(self,data,target):
+
+    def initialize_weights(self):
+        """ Initialize weights for perceptron. Don't forget the bias!
+
+        Returns:
+
+        """
+
+        return [0]
+
+    def score(self, X, y):
+        """ Return accuracy of model on a given dataset.
+        Args:
+            X (array-like): A 2D numpy array with data, excluding targets
+            y (array-like): A 2D numpy array with targets
+
+        Returns:
+
+        """
         pass
-    ### Given for your convenience
-    def shuffle_data(self,data,labels):
-        temp = np.hstack((data,labels))
-        np.random.shuffle(temp)
-        data,labels = temp[:, :data.shape[1]], temp[:, data.shape[1]:]
-        return data,labels
+
+    def _shuffle_data(self, X, y):
+        """ Shuffle the data! This _ prefix suggests that this method should only be called internally.
+            It might be easier to concatenate X & y and shuffle a single 2D array, rather than
+             shuffling X and y exactly the same way, independently.
+        """
+        pass
+
     ### Not required by sk-learn but required by us for grading. Returns the weights.
-    def getWeights(self):
+    def get_weights(self):
         pass
