d581a17f418cc1f76c9003118c6a01c1c6871b36
Brian Rasmussen <Brian.rasmussen55@gmail.com>
2019-10-30 18:10:13 -0600

    skeleton code KNN

---
+++ KNN/KNN.py
@@ -0,0 +1,50 @@
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from tools import shuffle
+
+class KNNClassifier(BaseEstimator,ClassifierMixin):
+
+    def __init__(self,columntype=[],weight_type='inverse_distance'): ## add parameters here
+        """
+        Args:
+            columntype for each column tells you if continues[real] or if nominal.
+            weight_type: inverse_distance voting or if non distance waiting option = ["no_weight","inverse_distance"]
+        """"
+        self.columntype = columntype
+        self.weight_type = weight_type
+
+
+    def fit(self,data,labels):
+    """ Fit the data; run the algorithm (for this lab really just saves the data :D)
+        Args:
+            X (array-like): A 2D numpy array with the training data, excluding targets
+            y (array-like): A 2D numpy array with the training targets
+        Returns:
+            self: this allows this to be chained, e.g. model.fit(X,y).predict(X_test)
+        """
+        return self
+    def predict(self,data):
+        """ Predict all classes for a dataset X
+        Args:
+            X (array-like): A 2D numpy array with the training data, excluding targets
+        Returns:
+            array, shape (n_samples,)
+                Predicted target values per element in X.
+        """
+        pass
+
+    #Returns the Mean score given input data and labels
+    def score(self,data,labels):
+         def score(self, X, y):
+        """ Return accuracy of model on a given dataset. Must implement own score function.
+        Args:
+            X (array-like): A 2D numpy array with data, excluding targets
+            y (array-like): A 2D numpy array with targets
+        Returns:
+            score : float
+                Mean accuracy of self.predict(X) wrt. y.
+        """
+
+        return 0
+
+
