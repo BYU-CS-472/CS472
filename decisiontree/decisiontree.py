b4c8b609b327a3aa92bd128798ecf4e815d43df8
Brian Rasmussen <Brian.Rasmussen55@gmail.com>
2020-03-09 10:24:09 -0600

    fixed spelling decision tree

---
+++ decisiontree/decisiontree.py
@@ -0,0 +1,64 @@
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+
+### NOTE: The only methods you are required to have are:
+#   * predict
+#   * fit
+#   * score
+
+class DTClassifier(BaseEstimator,ClassifierMixin):
+
+    def __init__(self,counts=None):
+        """ Initialize class with chosen hyperparameters.
+        Args:
+        Optional Args (Args we think will make your life easier):
+            counts: A list of Ints that tell you how many types of each feature there are
+        Example:
+            DT  = DTClassifier()
+            or
+            DT = DTClassifier(count = [2,3,2,2])
+            Dataset = 
+            [[0,1,0,0],
+            [1,2,1,1],
+            [0,1,1,0],
+            [1,2,0,1],
+            [0,0,1,1]]
+
+        """
+
+    def fit(self, X, y):
+        """ Fit the data; Make the Desicion tree
+
+        Args:
+            X (array-like): A 2D numpy array with the training data, excluding targets
+            y (array-like): A 2D numpy array with the training targets
+
+        Returns:
+            self: this allows this to be chained, e.g. model.fit(X,y).predict(X_test)
+
+        """
+
+        return self
+
+    def predict(self, X):
+        """ Predict all classes for a dataset X
+
+        Args:
+            X (array-like): A 2D numpy array with the training data, excluding targets
+
+        Returns:
+            array, shape (n_samples,)
+                Predicted target values per element in X.
+        """
+        pass
+
+
+    def score(self, X, y):
+        """ Return accuracy(Classification Acc) of model on a given dataset. Must implement own score function.
+
+        Args:
+            X (array-like): A 2D numpy array with data, excluding targets
+            y (array-like): A 2D numpy array of the targets 
+        """
+        return 0
+
