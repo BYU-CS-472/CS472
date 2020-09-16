9c65b6cf840892c7086d96f30745b893b388ebea
drk944 <64123074+drk944@users.noreply.github.com>
2020-09-15 18:35:53 -0600

    first edit

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -23,23 +23,51 @@ class PerceptronClassifier(BaseEstimator,ClassifierMixin):
         self.lr = lr
         self.shuffle = shuffle
 
-    def fit(self, X, y, initial_weights=None):
-        """ Fit the data; run the algorithm and adjust the weights to find a good solution
 
+    def fit(self, X, y, initial_weights=None):  # REQ
+        """ Fit the data; run the algorithm and adjust the weights to find a good solution
         Args:
             X (array-like): A 2D numpy array with the training data, excluding targets
             y (array-like): A 2D numpy array with the training targets
             initial_weights (array-like): allows the user to provide initial weights
-
         Returns:
             self: this allows this to be chained, e.g. model.fit(X,y).predict(X_test)
-
         """
-        self.initial_weights = self.initialize_weights() if not initial_weights else initial_weights
+        num_rows, num_cols = X.shape
+
+        new_col = np.ones(num_rows, 1) # Bias added to X
+        X = np.append(X, new_col, num_cols) # num_cols is the column that we want to append the 1's onto, not that it matters
+        num_cols = X.shape[1]   # Num cols updated after addition of bias
+
+        # self.initial_weights = self.initialize_weights() if not initial_weights else initial_weights
+        self.initial_weights = np.zeros(num_cols) # Weights initialized to 0
+        weights = self.initial_weights
+        delta_W = np.zeros(num_cols) # initializing delta ) to all zero's. They'll be updated later
+        
+        num_no_change_iters = 0
+
+        while(num_no_change_iters < (num_rows * 10)):    # 10 eppochs of no change multiplied by num patterns
+            for i in range(num_rows): # iterating over entire training set
+                net = 0 # reset net after iterations
+                output = 0
+                for j in range(num_cols): # iterating over pattern
+                    net = net + (X[i][j] * weights[j])
+                if net > 0:
+                    output = 1
+                for j in range(num_cols):
+                    delta_W[j] = self.lr *(y[i] - output) * X[i][j]  # Perceptron function to calculate delta W
+                if delta_W == np.zeros(j):
+                    num_no_change_iters = num_no_change_iters + 1
+                else:
+                    num_no_change_iters = 0
+
+                weights = np.add(weights, delta_W)  # Apply delta W to weight vector using Numpy
+            # First Eppoch completed
+            # ImplementMe shuffle the data
 
         return self
 
-    def predict(self, X):
+    def predict(self, X):   # REQ
         """ Predict all classes for a dataset X
 
         Args:
@@ -57,10 +85,10 @@ class PerceptronClassifier(BaseEstimator,ClassifierMixin):
         Returns:
 
         """
-
+            
         return [0]
 
-    def score(self, X, y):
+    def score(self, X, y):  # REQ
         """ Return accuracy of model on a given dataset. Must implement own score function.
 
         Args:
@@ -79,8 +107,12 @@ class PerceptronClassifier(BaseEstimator,ClassifierMixin):
             It might be easier to concatenate X & y and shuffle a single 2D array, rather than
              shuffling X and y exactly the same way, independently.
         """
+        arry = np.append(X, y, 1) # axis is 1 to refer to columns
+        np.random.shuffle(arry)
+
+
         pass
 
     ### Not required by sk-learn but required by us for grading. Returns the weights.
-    def get_weights(self):
+    def get_weights(self):  # REQ
         pass
